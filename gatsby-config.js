3e472a35aa5d51c8c6cc448280f411f687bc2fea
sandy <sandy.rav@onja.org>
2021-05-25 16:32:43 +0300

    fixed path to email

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,92 +1,93 @@
 module.exports = {
-    siteMetadata: {
-        title: `Sandy Portfolio`,
-        description: `
+  siteMetadata: {
+    title: `Sandy Portfolio`,
+    description: `
         I am a web developer focusing on front-end development based in Madagascar.
         
         I am self-motivated and I love learning new skills including coding, languages or anything else.
   `,
-        author: `Sandy Raveloarisoa`,
-        siteUrl: `https://github.com/Voninkazo/`,
-        social: {
-            twitter: '',
-            facebook: '',
-            github: `Voninkazo/`,
-            linkedin: ``,
-            email: "sandy.rav@onja.org",
-        },
+    author: `Sandy Raveloarisoa`,
+    siteUrl: `https://github.com/Voninkazo/`,
+    social: {
+      twitter: '',
+      facebook: '',
+      github: `Voninkazo/`,
+      linkedin: ``,
+      email: `sandy.rav@onja.org`,
     },
-    plugins: [{
-            resolve: `gatsby-source-filesystem`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
+              maxWidth: 970,
+              withWebp: true,
+              withAvif: true,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-katex`,
             options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
+              strict: `warn`,
             },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
             options: {
-                plugins: [{
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 970,
-                            withWebp: true,
-                            withAvif: true,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-katex`,
-                        options: {
-                            strict: `warn`,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-copy-linked-files`,
-                        options: {
-                            destinationDir: `files`,
-                            ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
-                        },
-                    },
-                    `gatsby-remark-prismjs`,
-                ],
+              destinationDir: `files`,
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
             },
-        },
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        // uncomment this and input the trackingId to enable google analytics
-        // {
-        // resolve: `gatsby-plugin-google-analytics`,
-        // options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-        // },
-        // },
-        `gatsby-plugin-feed`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `flexible-gatsby-starter`,
-                short_name: `flexible-gatsby`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
-        // `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sass`,
-    ],
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    // uncomment this and input the trackingId to enable google analytics
+    // {
+    // resolve: `gatsby-plugin-google-analytics`,
+    // options: {
+    // trackingId: `ADD YOUR TRACKING ID HERE`,
+    // },
+    // },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `flexible-gatsby-starter`,
+        short_name: `flexible-gatsby`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
 }
-
 
 // I am confident with HTML, CSS/SASS, Vanilla Javascript and React and I am having a desire to learn new web technology to add those skills that I currently own.
 
